3aff93bc8b2d4e41f1088e10f9f5083e1709e8ca
Oscar <oscar.ims93@gmail.com>
2019-11-14 21:34:30 -0600

    se creo agenda.js y coments on server.js, se agrego tambien la coneccion en mongoose

--- Server.js
+++ Server.js
@@ -1,9 +1,18 @@
+//** Imports libraries */
 const express = require("express")
 const bodyParser = require("body-parser")
-
+const mongoose = require('mongoose')
+//** Connection Mongoose */
+const CONNECTION_URL =
+  '  '  //**      <------------------------ la url */
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true }, err => {
+    if (!err) console.log('Te has conectado a la matrix')
+  })
+//** PORT */
 const PORT = 3000
+//** Init express */
 const apiSocio = express()
-
+//** SetUp BodyParser */
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
