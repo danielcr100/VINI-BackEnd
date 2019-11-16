401ec3c51dc43b593a243d5e3775103efb6ec601
Daniel Castaneda <danielcr100@gmail.com>
2019-11-15 19:21:26 -0600

    actualizacion de url para mongoose

--- models/Socio.js
+++ models/Socio.js
@@ -1,5 +1,32 @@
 const mongoose = require("mongoose")
-const CONNECTION_URL = "mongodb+srv://danielcr100:Cuowmr2c2GNGh5T1@clustervinis-6pbxe.gcp.mongodb.net/test?retryWrites=true&w=majority"
+const CONNECTION_URL = "mongodb+srv://danielcr100:5HchAlYBeEW2tCze@clustervinis-6pbxe.gcp.mongodb.net/test?retryWrites=true&w=majority"
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true }, err => {
     if (!err) console.log('Conexion exitosa')
 })
+
+const Schema = mongoose.Schema
+//Schema de base de datos
+const ServiciosSchema = new Schema(
+    {
+        nombre: {
+            type: String,
+            required: true
+        },
+        apellido: String,
+        telefono: String,
+        email: {
+            type: String
+        },
+        direccion: String,
+        direccion2: String,
+        pais: String,
+        provincia: String,
+        codigo_postal: Number,
+        guardar_info: String
+    },
+    { timestamps: true }
+)
+//Modelo de la base de datos
+const Pelicula = mongoose.model('Peliculas', peliculasSchema)
+
+module.exports = { Pelicula }
--- models/agendar.js
+++ models/agendar.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose')
-
-
-
+const CONNECTION_URL = "mongodb+srv://danielcr100:5HchAlYBeEW2tCze@clustervinis-6pbxe.gcp.mongodb.net/test?retryWrites=true&w=majority"
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true }, err => {
+  if (!err) console.log('Conexion exitosa')
+})
 
 
 const Schema = mongoose.Schema
@@ -15,11 +16,11 @@ const ServiciosSchema = new Schema(
     apellido: String,
     telefono: String,
     email: {
-        type: String
+      type: String
     },
     direccion: String,
     direccion2: String,
-    pais:String,
+    pais: String,
     provincia: String,
     codigo_postal: Number,
     guardar_info: String
